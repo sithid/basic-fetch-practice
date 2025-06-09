e4398ed3c90881da0c05eaf7599197602bee3bb3
James Glosser <demonicurges05@gmail.com>
2025-06-09 04:12:51 -0400

    CORS FIX: switched to using the Github API URL

--- public/js/index.js
+++ public/js/index.js
@@ -1,65 +1,105 @@
-//const dataUrl = "https://github.com/dan-collins-dev/dummy-data-fetching-repo/blob/main/data/users.json"
-const dataUrl = "https://raw.githubusercontent.com/dan-collins-dev/dummy-data-fetching-repo/main/data/users.json";
+// this url is the only one that would work. I tried the address from the assignment, I tried the raw.githubusercontent.com,
+// I finally got it working using the api.
+const dataUrl =
+  "https://api.github.com/repos/dan-collins-dev/dummy-data-fetching-repo/contents/data/users.json";
 
-const allUsersBtn = document.getElementById( "allUsersBtn" );
-const filteredUsersBtn = document.getElementById( "filteredUsersBtn" );
-const resetBtn = document.getElementById( "resetBtn" );
-const cardContainer = document.getElementById( "user-container" );
+const allUsersBtn = document.getElementById("allUsersBtn");
+const filteredUsersBtn = document.getElementById("filteredUsersBtn");
+const resetBtn = document.getElementById("resetBtn");
+const cardContainer = document.getElementById("user-container");
 
-allUsersBtn.addEventListener('click', allUsersClicked );
-filteredUsersBtn.addEventListener('click', filteredUsersClicked );
-resetBtn.addEventListener('click', resetClicked );
+allUsersBtn.addEventListener("click", allUsersClicked);
+filteredUsersBtn.addEventListener("click", filteredUsersClicked);
+resetBtn.addEventListener("click", resetClicked);
 
 function allUsersClicked() {
   const requestHeader = new Headers();
   requestHeader.append("Accept", "application/json");
   requestHeader.append("Content-Type", "application/json");
-  
-  const options = { 
+
+  const options = {
     method: "GET",
     headers: requestHeader,
   };
 
   fetch(dataUrl, options)
-    .then( response => {
-
-      if( !response.ok ) {
-        throw new Error(`Error fetching data: ${response.status}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error fetching data: ${response.status}`);
       }
 
       return response.json();
     })
-    .then( data => {
+    .then((data) => {
+      // data.content is base64 encoded.
+      // decode and convert to json.
+      const decodedContent = atob(data.content);
+      const jsonData = JSON.parse(decodedContent);
+
       const cards = [];
 
-      for( let user of data ) {
-        const card = createUserCard( user );
+      // process users
+      for (let user of jsonData) {
+        const card = createUserCard(user);
         cards.push(card);
       }
 
-      cardContainer.replaceChild(cards);
+      cardContainer.replaceChildren(...cards);
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error);
     });
 }
 
 function filteredUsersClicked() {
-  console.log("filteredUsersClicked");
+  const requestHeader = new Headers();
+  requestHeader.append("Accept", "application/json");
+  requestHeader.append("Content-Type", "application/json");
+
+  const options = {
+    method: "GET",
+    headers: requestHeader,
+  };
+
+  fetch(dataUrl, options)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error fetching data: ${response.status}`);
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      const decodedContent = atob(data.content);
+      const jsonData = JSON.parse(decodedContent);
+
+      const cards = [];
+
+      for (let user of jsonData) {
+        // filter users without enough experience
+        if (user.yearsEmployed < 10) {
+          const card = createUserCard(user);
+          cards.push(card);
+        }
+      }
+
+      cardContainer.replaceChildren(...cards);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 function resetClicked() {
   cardContainer.replaceChildren(); // no args clears the children
-  console.log("resetClicked");
 }
 
-function createUserCard ( user ) {
-  const userCard = document.createElement('div');
+function createUserCard(user) {
+  const userCard = document.createElement("div");
 
-  userCard.classList.add('user-card');
-  userCard.setAttribute('data-user-id', user.id );
-  userCard.innerHTML =
-  `
+  userCard.classList.add("user-card");
+  userCard.setAttribute("data-user-id", user.id);
+  userCard.innerHTML = `
     <p>${user.firstName} ${user.lastName}</p>
     <p>${user.email}</p>
     <p>${user.companyName}</p>
--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.static("./public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
-    console.log(`Server is running http://localhost:${port}`);
-    console.log("Press Ctrl+C to end this process.");
+  console.log(`Server is running http://localhost:${port}`);
+  console.log("Press Ctrl+C to end this process.");
 });
